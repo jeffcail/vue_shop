2af0299a694504b3b1ed68feb7fc2e41e548d2f5
cc <cc@ccdeMacBook-Pro.local>
2022-02-05 20:18:56 +0800

    用户列表页面布局&&用户列表相关功能

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
+import Users from '../components/user/Users.vue'
+import Permission from '../components/permission/Permission.vue'
+import Role from '../components/permission/Role.vue'
 
 // 导入全局样式
 import '../assets/css/global.css'
@@ -11,7 +15,17 @@ Vue.use(VueRouter)
 const routes = [
   { path: '/', redirect: '/login'},
   { path: '/login', component: Login },
-  { path: '/home', component: Home }
+  { 
+    path: '/home', 
+    component: Home, 
+    redirect: '/welcome',
+    children: [
+      { path: '/welcome', component: Welcome },
+      { path: '/users', component: Users },
+      { path: '/roles', component: Role },
+      { path: '/rights', component:Permission }
+    ]
+  }
 ]
 
 const router = new VueRouter({
