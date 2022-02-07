42d4e813639c886894fac2e2dc16214c996834cf
cc <cc@ccdeMacBook-Pro.local>
2022-02-07 16:35:05 +0800

    数据报表

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Params from '../components/goods/Params.vue'
 import List from '../components/goods/List.vue'
 import Add from '../components/goods/Add.vue'
 import Order from '../components/order/Order.vue'
+import Report from '../components/report/Report.vue'
 
 // 导入全局样式
 import '../assets/css/global.css'
@@ -33,7 +34,8 @@ const routes = [
       { path: '/params', component: Params },
       { path: '/goods', component: List },
       { path: '/goods/add', component: Add },
-      { path: '/orders', component: Order }
+      { path: '/orders', component: Order },
+      { path: '/reports', component: Report}
     ]
   }
 ]
